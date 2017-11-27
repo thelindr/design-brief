362956068b01c9ef5c6f000f9d5d12185194819e
Therese Lindroth <31589557+thelindr@users.noreply.github.com>
2017-11-27 13:48:42 +0100

    set structure

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -5,8 +5,50 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        Find me in src/app.js!
-      </div>
+
+        <div className="Menu">
+          <li>Creativity</li>
+        </div>
+
+
+          <div className="Header">
+            <div className="leftColumn">
+              <h1>Interesting ideas that set your mind in motion.</h1>
+                <p>Hear directly from the people who know it best. From tech to politics to creativity and more - whatever your interest, we've got you covered</p>
+                  <div className="buttons">
+                    <button type="firstbutton">Get started</button>
+                    <button type="secondbutton">Learn more</button>
+                  </div>
+                </div>
+              </div>
+
+              <div className="contentBoxes">
+                <div id="box" className="firstBox">
+                    <div className="textbox">
+                  <h2>The Boy Who Tamed the Sea</h2>
+                  <p>Lorem ipsum....</p>
+                    </div>
+                </div>
+                <div id="box" className="secondBox">
+                    <div className="textbox">
+                  <h2>The role of human emotions...</h2>
+                  <p>Lorem ipsum....</p>
+                    </div>
+                </div>
+                <div id="box" className="thirdBox">
+                    <div className="textbox">
+                  <h2>The Boy Who Tamed the Sea</h2>
+                  <p>Lorem ipsum....</p>
+                    </div>
+                </div>
+                <div id="box" className="fourthBox">
+                    <div className="textbox">
+                  <h2>The Boy Who Tamed the Sea</h2>
+                  <p>Lorem ipsum....</p>
+                    </div>
+                </div>
+              </div>
+            </div>
     )
   }
 
