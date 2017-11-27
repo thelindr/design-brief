4cda544a519fb7b4342dcfd489b1d943c30538ec
Therese Lindroth <31589557+thelindr@users.noreply.github.com>
2017-11-27 17:03:46 +0100

    almost finished

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -7,7 +7,18 @@ class App extends React.Component {
       <div className="App">
 
         <div className="Menu">
+          <ul>
+          <li id="bold">Home</li>
+          <li>Technology</li>
           <li>Creativity</li>
+          <li>Entrepreneurship</li>
+          <li>Self</li>
+          <li>Culture</li>
+          <li>Digital Design</li>
+          <li>Popular on Medium</li>
+          <li>Politics</li>
+          <button id="menubutton" type="button">More</button>
+          </ul>
         </div>
 
 
@@ -16,8 +27,8 @@ class App extends React.Component {
               <h1>Interesting ideas that set your mind in motion.</h1>
                 <p>Hear directly from the people who know it best. From tech to politics to creativity and more - whatever your interest, we've got you covered</p>
                   <div className="buttons">
-                    <button type="firstbutton">Get started</button>
-                    <button type="secondbutton">Learn more</button>
+                    <button id="firstbutton" type="button">Get started</button>
+                    <button id="secondbutton" type="button">Learn more</button>
                   </div>
                 </div>
               </div>
@@ -26,25 +37,29 @@ class App extends React.Component {
                 <div id="box" className="firstBox">
                     <div className="textbox">
                   <h2>The Boy Who Tamed the Sea</h2>
-                  <p>Lorem ipsum....</p>
+                  <p>At 5:30 am, on any motning I can handle it, I'll slip out of the dark mouth of my covers and creep through the silver lig...</p>
+                    <div className="byline">
+                    <img src="/images/writer-1.png"></img>
+                      <h3>Carl Moore</h3>
+                      </div>
                     </div>
                 </div>
                 <div id="box" className="secondBox">
                     <div className="textbox">
-                  <h2>The role of human emotions...</h2>
-                  <p>Lorem ipsum....</p>
+                      <h2>The role of human emotions...</h2>
+                      <p>Lorem ipsum....</p>
                     </div>
                 </div>
                 <div id="box" className="thirdBox">
                     <div className="textbox">
-                  <h2>The Boy Who Tamed the Sea</h2>
-                  <p>Lorem ipsum....</p>
+                      <h2>The Boy Who Tamed the Sea</h2>
+                      <p>Lorem ipsum....</p>
                     </div>
                 </div>
                 <div id="box" className="fourthBox">
                     <div className="textbox">
-                  <h2>The Boy Who Tamed the Sea</h2>
-                  <p>Lorem ipsum....</p>
+                      <h2>The Boy Who Tamed the Sea</h2>
+                      <p>Lorem ipsum....</p>
                     </div>
                 </div>
               </div>
